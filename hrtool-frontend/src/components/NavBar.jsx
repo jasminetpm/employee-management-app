d88d7b01e0928e952d510f4687defc8501337e18
Jasmine Tan <31689454+jasminetpm@users.noreply.github.com>
2025-12-17 17:37:40 +0800

    Added icon to browser tab and NavBar

--- hrtool-frontend/src/components/NavBar.jsx
+++ hrtool-frontend/src/components/NavBar.jsx
@@ -1,11 +1,22 @@
 import { AppBar, Toolbar, Typography, Button, Container, Box } from '@mui/material';
 import { Link as RouterLink } from 'react-router-dom';
 
+import icon from '../assets/icon.png';
+
 const NavBar = () => {
     return (
         <AppBar position="static">
             <Container maxWidth="lg">
                 <Toolbar disableGutters>
+                    <Box
+                        component="img"
+                        sx={{
+                            height: 45,  // Adjusts the size of the icon
+                            mr: 1.5        // Adds margin-right (space) between icon and text
+                        }}
+                        alt="Logo"
+                        src={icon}
+                    />
                     <Typography variant="h6" component="div" sx={{ flexGrow: 1, fontWeight: 'bold' }}>
                         HR Tool
                     </Typography>
